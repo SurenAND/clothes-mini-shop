da9574d8687d260461f38f1aa54a445453cb08e8
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-20 12:20:00 +0330

    create open cart modal function

--- script.js
+++ script.js
@@ -68,3 +68,32 @@ function createCartSection() {
   shoppingCart.append(cartCounter, cartIcon);
   mainSection.append(shoppingCart);
 }
+
+function showCart() {
+  // open cart modal
+  const cartModalOverlay = document.querySelector(".cart-modal-overlay");
+
+  if (cartModalOverlay.style.transform === "translateX(-200%)") {
+    cartModalOverlay.style.transform = "translateX(0)";
+  } else {
+    cartModalOverlay.style.transform = "translateX(-200%)";
+  }
+
+  changeTotalPrice();
+
+  // end of open cart modal
+
+  // close cart modal
+  const closeBtn = document.querySelector("#close-btn");
+
+  closeBtn.addEventListener("click", () => {
+    cartModalOverlay.style.transform = "translateX(-200%)";
+  });
+
+  cartModalOverlay.addEventListener("click", (e) => {
+    if (e.target.classList.contains("cart-modal-overlay")) {
+      cartModalOverlay.style.transform = "translateX(-200%)";
+    }
+  });
+  // end of close cart modal
+}
