5ea6fd44aa224ee2836c1cf0bef7b357b8ed0c58
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-20 12:10:59 +0330

    create products array

---
+++ script.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, price: "300", image: "image-1" },
+  { id: 2, price: "420", image: "image-2" },
+  { id: 3, price: "240", image: "image-3" },
+  { id: 4, price: "560", image: "image-4" },
+  { id: 5, price: "1150", image: "image-5" },
+];
